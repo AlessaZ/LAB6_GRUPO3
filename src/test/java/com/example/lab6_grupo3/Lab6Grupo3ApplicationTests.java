8ca48e9ef0228220150d60fb78dd7d52b98207f0
Alessandra Morales <azmoralesm@pucp.edu.pe>
2022-05-31 20:43:39 -0500

    Primer commit

---
+++ src/test/java/com/example/lab6_grupo3/Lab6Grupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6Grupo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
